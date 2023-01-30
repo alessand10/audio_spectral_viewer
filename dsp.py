2ae4c7d90660248e32a0a3745a15f84ae86dc14b
alessand10 <alessandrogenovese1@hotmail.com>
2023-01-29 22:14:05 -0500

    Update to FFT

--- dsp.py
+++ dsp.py
@@ -3,6 +3,7 @@ import math
 '''
     Follows the mathematical definition of the discrete Fourier transform.
 '''
+
 def dft(input):
     output = []
     for m in range(0, len(input)):
@@ -16,6 +17,31 @@ def dft(input):
         output.append(sum)
     return output 
 
+def twiddle(N, divisor, exp_mult):
+    exp = 2 * math.pi * (divisor/N) * exp_mult
+    return math.cos(exp) - 1.0j*math.sin(exp)
 
+
+def A(m, x):
+    N_div_2 = len(x) / 2 - 1
+    result = 0 + 0j
+    for n in range(0, N_div_2):
+        result += x[2*n] * (twiddle(2, len(x)/2, m * n))
+
+    return result
+
+def B(m, x):
+    N_div_2 = len(x) / 2 - 1
+    result = 0 + 0j
+    for n in range(0, N_div_2):
+        result += x[2*n + 1] * (2, twiddle(len(x)/2, m * n))
+
+    return result
+
+
+
+# Ensure the size of the input is a power of 2
 def fft(input):
-    
+    output = []
+    N = len(input)
+    levels = math.log2(input)
---
+++ test.py
@@ -0,0 +1,13 @@
+import dsp
+fs = 100
+N = 1024
+
+# t = []
+# sig = []
+# for i in range(0, N):
+#     t.append(i * 1/fs)
+#     sig.append(sint[-1])
+
+
+# res = dsp.dft(sig)
+# a = 3
